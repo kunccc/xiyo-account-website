2c46375e7af503f3f6bac7f5c0a8a0c0d6c0a4d6
Quincy <kuncc@foxmail.com>
2021-02-24 18:14:34 +0800

    deploy

---
+++ precache-manifest.4df6db3faff2de89c3d902875ea1d7f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023991b427f893175833",
+    "url": "/xiyo-account-website/css/app.8ece1c5c.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/xiyo-account-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/xiyo-account-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "2f5960598c244f3ed2194aff0b1a6289",
+    "url": "/xiyo-account-website/index.html"
+  },
+  {
+    "revision": "023991b427f893175833",
+    "url": "/xiyo-account-website/js/app.a4669e0f.js"
+  },
+  {
+    "revision": "8d7b2b78cf0a89f6ef36",
+    "url": "/xiyo-account-website/js/chunk-vendors.cdf441ad.js"
+  },
+  {
+    "revision": "0950f5405a5fe3501a3989ccde1150c2",
+    "url": "/xiyo-account-website/manifest.json"
+  },
+  {
+    "revision": "dea8396f0494813ccf5b2062cd57f12c",
+    "url": "/xiyo-account-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiyo-account-website/robots.txt"
+  }
+]);
